5252ba5bf91a6c2f95c101a6da73b50e3afd26ed
Mehdi Akacem <makacem@e2r9p10.1337.ma>
2022-12-17 18:23:19 +0100

    added redirections

--- parser/ft_check_redirections.c
+++ parser/ft_check_redirections.c
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 15:34:01 by makacem           #+#    #+#             */
-/*   Updated: 2022/12/17 17:23:55 by makacem          ###   ########.fr       */
+/*   Updated: 2022/12/17 18:20:24 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,25 @@ int ft_check_redirections(t_token *token_list)
 	}
 	token = token_list;
 	token = token->next;
-	count = 0;
 	while (token != NULL)
 	{
-		while (token->type != REDIRECTION && token->next != NULL)
+		count = 0;
+		while (token->type == REDIRECTION)
 		{
-			if (token->type == PIPE)
-				return(1);
+			count++;
+			if (count == 3)
+				return (1);
 			token = token->next;
 		}
-		
+		token = token->next;
+	}
+	token = token_list;
+	token = token->next;
+	while (token != NULL)
+	{
+		if ((ft_strncmp(token->name, ">", 1) == 0 && ft_strncmp(token->next->name, "<", 1) == 0) ||
+				(ft_strncmp(token->name, "<", 1) == 0 && ft_strncmp(token->next->name, ">", 1) == 0))
+			return (1);
 		token = token->next;
 	}
 	return (0);
--- parser/ft_pars.c
+++ parser/ft_pars.c
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:22:31 by makacem           #+#    #+#             */
-/*   Updated: 2022/12/17 17:29:08 by makacem          ###   ########.fr       */
+/*   Updated: 2022/12/17 18:20:55 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,13 @@ int	ft_pars(t_token *token_list)
 		return (1);
 	else if (ft_check_pipes(token_list) == 1)
 		return (1);
-	// else if (ft_check_redirections(token_list) == 1)
-	// 	return (1);
+	else if (ft_check_redirections(token_list) == 1)
+		return (1);
 	return (0);
 }
 
 int	ft_pars_error(void)
 {
-	printf("pars error\n");
+	printf("parse error\n");
 	return (1);
 }
