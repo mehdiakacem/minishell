b1f3c69692aae5dadfae45d92039f4d14f88b351
Nouhaila Moussammi <nmoussam@e1r10p11.1337.ma>
2022-12-22 13:38:59 +0100

    add sort

--- execution/builtins/export.c
+++ execution/builtins/export.c
@@ -6,17 +6,44 @@
 /*   By: nmoussam <nmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:41:35 by nmoussam          #+#    #+#             */
-/*   Updated: 2022/12/21 23:58:03 by nmoussam         ###   ########.fr       */
+/*   Updated: 2022/12/22 13:35:18 by nmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+t_env *sort_env(t_env *head)
+{    
+    t_env *temp;
+    t_env *temp_next;
+    t_env *temp_head;
+
+    temp_head = head;
+    int  i;
+    i = 0;
+    while (temp_head != NULL && temp_head->next != NULL)
+    {
+        if (temp_head->var[0] > temp_head->next->var[0])
+        {
+            temp = temp_head;
+            temp_head = temp_head->next;
+            temp->next = temp_head->next;
+            temp_head->next = temp;
+        }
+        // printf("%c   %c\n\n", temp_head->var[0], temp_head->next->var[0]);
+        temp_head = temp_head->next;
+    }
+    return(head);
+}
+
+
 void export(int argc, char **argv, char **en) //nzid f env and tertib
 {
+    t_env   *temp;
     t_env   *tmp;
     
-    tmp = get_env(en);
+    temp = get_env(en);
+    tmp = sort_env(temp);
     if(argc > 2 && (ft_isdigit(argv[2][0]) == 1 || (argv[2][0] == '-' && argv[2][1] == '\0')))
         printf("minishell: export: '%s': not a valid identifier\n", argv[2]); //env -- # - () &
     if (argc > 2 && argv[2][0] == '-' && argv[2][1] != '\0')
@@ -39,5 +66,6 @@ void export(int argc, char **argv, char **en) //nzid f env and tertib
 }
 int main(int argc, char **argv, char **en)
 {
+    // t_env *cmd = get_env(en);
     export(argc, argv, en);
 }
--- execution/builtins/test.c
+++ execution/builtins/test.c
@@ -6,7 +6,7 @@
 /*   By: nmoussam <nmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 23:30:02 by nmoussam          #+#    #+#             */
-/*   Updated: 2022/12/21 23:30:13 by nmoussam         ###   ########.fr       */
+/*   Updated: 2022/12/22 13:24:58 by nmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,3 @@
 
 #include<stdio.h>
 
-// chdir function is declared
-// inside this header
-#include<unistd.h>
-int main()
-{
-	char s[100];
-
-	// printing current working directory
-	printf("%s\n", getcwd(s, 100));
-
-	// using the command
-	chdir("..");
-
-	// printing current working directory
-	printf("%s\n", getcwd(s, 100));
-
-	// after chdir is executed
-	return 0;
-}
