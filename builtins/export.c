027d61d3ff80b5bb3f6160004826598cd2097dcb
Nouhaila Moussammi <nmoussam@e1r10p13.1337.ma>
2022-12-26 21:47:02 +0100

    mzl mkmlt export

--- builtins/export.c
+++ builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: nmoussam <nmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:41:35 by nmoussam          #+#    #+#             */
-/*   Updated: 2022/12/26 18:34:03 by nmoussam         ###   ########.fr       */
+/*   Updated: 2022/12/26 21:46:29 by nmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,31 +46,78 @@ t_env *sort_env(t_env *head)
     return(head);
 }
 
+int	find_ch(char *str, char ch)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+			return (1);
+		i++;
+	}
+	return(0);
+}
+
+
 
 void export(t_treenode *tree, char **en) //nzid f env and tertib
 {
     t_env   *temp;
     t_env   *tmp;
+    int     val;
+    int i;
 
+    i = 1;
+    val = 0;
     temp = get_env(en);
     tmp = sort_env(temp);
-    if(tree->nb_cmd > 1 && (ft_isdigit(tree->cmd[1][0]) == 1 || (tree->cmd[1][0] == '-' && tree->cmd[1][1] == '\0')))
-        printf("minishell: export: '%s': not a valid identifier\n", tree->cmd[1]); //env -- # - () &
-    if (tree->nb_cmd > 1 && tree->cmd[1][0] == '-' && tree->cmd[1][1] != '\0')
+    if (tree->nb_cmd > 1 && tree->cmd[1][0] == '-' && tree->cmd[1][1] != '-' && tree->cmd[1][1] != '\0')
     {
-        printf("minishell: export: -%c: invalid option\nexport: usage: export [-nf] [name[=value] ...] or export -p", tree->cmd[1][1]);
+        printf("minishell: export: -%c: invalid option\nexport: usage: export [-nf] [name[=value] ...] or export -p\n", tree->cmd[1][1]);
+        return ;
     }
-    else if(ft_strcmp(tree->cmd[0], "export") == 0) 
+    if(tree->nb_cmd > 1 && ((tree->cmd[1][0] == '-' && tree->cmd[1][1] != '-') || ft_isdigit(tree->cmd[1][0]) == 1 ))
+    {
+        printf("minishell: export: '%s': not a valid identifier\n", tree->cmd[1]);
+        return ;
+    }
+	while (i < tree->nb_cmd)
+	{
+			if (find_ch(tree->cmd[i], '=') == 1)
+			{
+				ft_lstadd_back(&tmp, ft_lstnew(tree->cmd[i]));
+       			// while(tmp)
+				// {
+				// 	if(tmp->content)
+				// 		printf("declare -x %s=\"%s\"\n", tmp->var, tmp->content);
+				// 	tmp = tmp->next;
+				// }
+			}
+			i++;
+		}
+        
+    
+     if(ft_strcmp(tree->cmd[0], "export") == 0 && (tree->cmd[1] == NULL || (tree->cmd[1][0] == '-' && tree->cmd[1][1] == '-') || tree->cmd[1][0] == '#'))
     {      
         while(tmp)
         {
             if(tmp->content)
             {
-                printf("declare -x ");
-                printf("%s",tmp->var);
-                printf("%s\n", tmp->content);
+                val = 1;
+                printf("declare -x %s=%s\n", tmp->var, tmp->content);
             }
             tmp = tmp->next;
         }
     }
+    
 }
+
+
+
+// void	add_export(t_treenode *tree)
+// {
+// 	printf("declare -x ");
+// 	ft_lstadd_back(tree->env, ft_lstnew())
+// }
--- utils/ft_lstnew.c
+++ utils/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: nmoussam <nmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 22:16:01 by nmoussam          #+#    #+#             */
-/*   Updated: 2022/12/26 15:19:34 by nmoussam         ###   ########.fr       */
+/*   Updated: 2022/12/26 21:35:04 by nmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_env	*ft_lstnew(char *lst)
 		return (NULL);
     new->var = ft_substr(lst, 0, ft_strlen(lst) - ft_strlen(ft_strchr(lst, '=')));
 	len = ft_strlen(lst) - ft_strlen(ft_strchr(lst, '='));
-	new->content = ft_substr(lst, ft_strlen(lst) - ft_strlen(ft_strchr(lst, '=')), ft_strlen(lst+len));
+	new->content = ft_substr(lst, ft_strlen(lst) - ft_strlen(ft_strchr(lst, '=')) + 1, ft_strlen(lst+len+1));
 	new->next = NULL;
 	return (new);
 }
