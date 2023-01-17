736403055d9d0498583b75875c6d698747fa8c0d
mehdiakacem <mehdiakacem20@gmail.com>
2023-01-17 22:35:30 +0100

    norm done but main

--- utils/ft_free_tree.c
+++ utils/ft_free_tree.c
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 21:59:00 by makacem           #+#    #+#             */
-/*   Updated: 2023/01/17 22:01:03 by makacem          ###   ########.fr       */
+/*   Updated: 2023/01/17 22:34:11 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	ft_free_tree(t_treenode *root)
 {
-	t_treenode *left;
-	t_treenode *tmp;
+	t_treenode	*left;
+	t_treenode	*tmp;
 
 	left = root;
 	if (root == NULL)
-		return;
+		return ;
 	while (left != NULL)
 	{
 		if (left->right != NULL )
-		 	free(left->right->cmd);
+			free(left->right->cmd);
 		free(left->right);
 		left = left->left;
 	}
